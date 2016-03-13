9c4798c495c7451aa6d59f6b0c7414ac1ce8e235
Nathaniel Miller <dukeoflaser@gmail.com>
2016-03-13 20:12:51 +0000

    Add Pokemon class. Make tests pass.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,33 @@
 class Pokemon
+  attr_reader :id, :name, :type, :db
+  
+  def initialize(id, name, type, db)
+    @id = id;
+    @name = name
+    @type = type
+    @db = db
+  end
+  
+  def self.save(name, type, db)
+    db.execute(
+      "INSERT INTO 
+        pokemon (name, type) 
+      VALUES
+        ('#{name}', '#{type}');"
+      )
+  end
+  
+  def self.find(id, db)
+    found_array = db.execute(
+      "SELECT 
+        id, name, type
+      FROM 
+        pokemon
+      WHERE 
+        id = #{id};"
+    )
+                
+    found_array.first            
+  end
+  
 end
